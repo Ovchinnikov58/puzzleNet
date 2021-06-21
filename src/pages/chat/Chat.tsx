6fd4625a8380322f2bcf3fdb443c274cb8223bae
Alexander Ovchinnikov <aleksandr.ovchinnikov@corp.codeinside.ru>
2021-06-21 16:24:49 +0300

    chat refactoring

--- src/pages/chat/Chat.tsx
+++ src/pages/chat/Chat.tsx
@@ -12,6 +12,7 @@ import './chat.scss'
 const Chat: FC = (): JSX.Element => {
   const [messageHistory, setMessageHistory] = useState([] as Array<Message | null>)
   const [messageText, setMessageText] = useState('')
+
   const { sendMessage, lastMessage, readyState } = useWebSocket(WS_CHANNEL)
   const { id, user } = useSelector((s: RootState): AuthState => s.auth)
 
@@ -23,64 +24,73 @@ const Chat: FC = (): JSX.Element => {
     [ReadyState.UNINSTANTIATED]: 'Uninstantiated',
   }[readyState]
 
-  useEffect(() => {
-    const mesObj = lastMessage ? JSON.parse(lastMessage?.data) : null
-    setMessageHistory([...messageHistory, mesObj])
-  }, [lastMessage])
-
-  useEffect(() => {
-    const el = document.querySelector('.chat__textarea')
-    if (el) el.scrollTop = 9999
-  }, [messageHistory])
-
-  useEffect(() => {
-    // if (connectionStatus === 'Open') toast('Соединение установлено', { type: 'success' })
-    if (connectionStatus === 'Closed') toast('Соединение не установлено', { type: 'error' })
-  }, [connectionStatus])
+  useEffect(parseMessages, [lastMessage])
+  useEffect(autoScrollChat, [messageHistory])
+  useEffect(checkConnectionStatus, [connectionStatus])
 
   return (
     <div className="chat">
-      {/* <span>The WebSocket is {connectionStatus}</span> */}
-      {connectionStatus === 'Connecting' ? <CircularProgress /> : null}
-      <div className="chat__textarea">
-        <ul className="chat__message-list">
-          {messageHistory.map((message) => (
-            <>
-              {message ? (
-                <li key={Math.random()} className={clsx('chat__message', id === message?.id && 'chat__message_self')}>
-                  {message?.user}: {message?.message}
-                </li>
-              ) : null}
-            </>
-          ))}
-        </ul>
-      </div>
-      <div className="chat__input-block">
-        <TextField
-          id="messageText"
-          label="Текст сообщения:"
-          className="chat__input"
-          variant="outlined"
-          autoFocus
-          required
-          value={messageText}
-          onChange={(e) => setMessageText(e.target.value)}
-        />
-        <button
-          className="chat__sendBtn"
-          type="button"
-          onClick={handleClickSendMessage}
-          disabled={readyState !== ReadyState.OPEN}
-        >
-          <SendIcon className="sendIcon" />
-        </button>
-      </div>
+      {connectionStatus === 'Connecting' ? <CircularProgress className="chat__loader" /> : null}
+      {connectionStatus === 'Open' ? (
+        <>
+          <div className="chat__textarea">
+            <ul className="chat__message-list">
+              {messageHistory.map((message) => (
+                <>
+                  {message ? (
+                    <li
+                      key={Math.random()}
+                      className={clsx('chat__message', id === message?.id && 'chat__message_self')}
+                    >
+                      {message?.user}: {message?.message}
+                    </li>
+                  ) : null}
+                </>
+              ))}
+            </ul>
+          </div>
+          <div className="chat__input-block">
+            <TextField
+              id="messageText"
+              label="Текст сообщения:"
+              className="chat__input"
+              variant="outlined"
+              autoFocus
+              required
+              value={messageText}
+              onChange={(e) => setMessageText(e.target.value)}
+            />
+            <button
+              className="chat__sendBtn"
+              type="button"
+              onClick={handleClickSendMessage}
+              disabled={readyState !== ReadyState.OPEN}
+            >
+              <SendIcon className="sendIcon" />
+            </button>
+          </div>
+        </>
+      ) : null}
     </div>
   )
 
   function handleClickSendMessage() {
     if (messageText) sendMessage(`{"id": ${id}, "user": "${user}", "message": "${messageText}"}`)
     setMessageText('')
+  }
+
+  function parseMessages() {
+    const mesObj = lastMessage ? JSON.parse(lastMessage?.data) : null
+    setMessageHistory([...messageHistory, mesObj])
+  }
+
+  function autoScrollChat() {
+    const el = document.querySelector('.chat__textarea')
+    if (el) el.scrollTop = 9999
+  }
+
+  function checkConnectionStatus() {
+    if (connectionStatus === 'Closed') toast('Соединение не установлено', { type: 'error' })
   }
 }
 
